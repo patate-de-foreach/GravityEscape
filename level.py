651ed78242605e86cfad245ca0cd10b5ce269b82
aleks <alexy.dasilva@icloud.com>
2023-09-19 17:24:04 +0200

    Collisions

--- level.py
+++ level.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 
 from mapManager import *
@@ -5,11 +7,12 @@ from ennemiFactory import *
 
 
 class Level:
-    def __init__(self, screen,tile_size):
+    def __init__(self, num_lvl, screen, mapManager, enemmiFactory):
         self.screen = screen
-        
-        self.mapManager
-        self.ennemiFactory
-        
+        self.mapManager = mapManager
+        self.ennemiFactory = enemmiFactory
+        with open('names' + num_lvl + ".csv", newline='') as csvfile:
+            self.csv = csv.DictReader(csvfile)
+        self.tiles = pygame.image.load('names' + num_lvl + '.png')
+        self.obstacle = json.load('names' + num_lvl + '.json')
 
-    
