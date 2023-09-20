51eb1c0bd67c77a34be9ce4351d7505060f0b600
Hugo <hugo.cacciatori.pro@gmail.com>
2023-09-20 20:13:18 +0200

    rotation du player

--- audio_manager.py
+++ audio_manager.py
@@ -10,36 +10,29 @@ class AudioManager:
 
     def init_player_sfx(self):
         self.gravity = mixer.Sound("assets/audio/sfx/gravity.mp3")
-        self.player_sounds = {
-            "gravity":self.gravity
-        }
+        self.player_sounds = {"gravity": self.gravity}
         for player_sound in self.player_sounds.values():
             player_sound.set_volume(self.sfx_level)
-    
+
     def init_mixer(self):
         mixer.init()
         mixer.music.set_volume(self.bgm_level)
-        self.bgm_paths = {
-            "roaming":"assets/audio/bgm/roaming.mp3"
-        }
+        self.bgm_paths = {"roaming": "assets/audio/bgm/roaming.mp3"}
 
-    ## Change la musique jouée 
+    ## Change la musique jouée
     # si loop = -1, la musique tourne en boucle
     # si une intro est donnée, joue l'intro et enchaine avec la musique
     def play_bgm(self, bgmName, loop=0, introName=""):
-        if introName != "" :
+        if introName != "":
             mixer.music.load(self.bgm_paths[introName])
             mixer.music.queue(self.bgm_paths[bgmName], loops=loop)
         else:
             mixer.music.load(self.bgm_paths[bgmName])
             mixer.music.play(loops=loop)
-    
+
     ## Arrête la musique en cours avec une option fadeout paramétrable
     def stop_bgm(self, fadeout=False, fadeout_time=1):
         if fadeout:
             mixer.music.fadeout(fadeout_time)
         else:
             mixer.music.stop()
-        
-        
-    
--- game.py
+++ game.py
@@ -1,22 +1,23 @@
-import pygame,sys
+import pygame, sys
 
 import game, player, ennemi, ennemiFactory, obstacle, level, mapManager, start
+
 
 class Game:
     def __init__(self, screen):
         SCREEN_WIDTH, SCREEN_HEIGHT = pygame.display.get_surface().get_size()
         self.clock = pygame.time.Clock()
-        self.Player = player.Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,screen, "CLAVIER", self.clock)
-        
+        self.Player = player.Player(
+            SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, screen, "CLAVIER", self.clock
+        )
+
         # Initialisation constructeur des game_state
         self.main_menu = start.Start(screen)
-        self.level1 = level.Level(1,screen,self.Player, self.clock)
-        
+        self.level1 = level.Level(1, screen, self.Player, self.clock)
+
         # Début du jeu state initial
         self.set_state("main_menu")
 
-        
-    
     def eventHandler(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -25,25 +26,28 @@ class Game:
 
     def update(self):
         # Va chercher le current_state et display sa fonction de run
-        if self.current_state_object != None and self.current_state_object.is_finished == True:
+        if (
+            self.current_state_object != None
+            and self.current_state_object.is_finished == True
+        ):
             self.set_state(self.current_state_object.redirect)
 
     def run(self):
         while True:
-            self.eventHandler()# event fermeture fenetre
+            self.eventHandler()  # event fermeture fenetre
             self.current_state_object = self.get_current_state_object()
             # Lance la boucle principale de l'état en cours
             self.current_state_object.run()
 
             # Vérifie si l'état est terminée
-            self.update() 
+            self.update()
             pygame.display.update()
             self.clock.tick(60)
-    
-    def set_state(self,new_state):
+
+    def set_state(self, new_state):
         self.current_state = new_state
         self.current_state_object = self.get_current_state_object()
-        
+
     def get_current_state_object(self):
         if self.current_state == "main_menu":
             return self.main_menu
